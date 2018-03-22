659cdf9de73d2c542e3b796838065bf4f41ad074
SenseLo <lnfuyuankai@126.com>
2018-03-22 13:34:43 +0800

    第一次提交

---
+++ app/src/main/java/it/fuyk/com/androidrecord/MainActivity.java
@@ -0,0 +1,18 @@
+package it.fuyk.com.androidrecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+    /*
+    * 主界面模板
+    * */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
